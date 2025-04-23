cc56ba97174e42d30e1d7a4d4d5db7170613165e
Hoang Tien Lang <langhoangtien@gmail.com>
2025-04-23 15:49:50 +0700

    dsfsdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
@@ -11,6 +13,7 @@ import {
   Info,
 } from "lucide-react";
 import { COMPANY_NAME } from "@/config";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: COMPANY_NAME,
@@ -25,6 +28,34 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">
+        {/* Facebook Pixel Script */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+              !function (f, b, e, v, n, t, s) {
+      if (f.fbq) return; n = f.fbq = function () {
+        n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      };
+      if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+      n.queue = []; t = b.createElement(e); t.async = !0;
+      t.src = v; s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s)
+    }(window, document, 'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '981817467469013');       
+    fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Facebook Pixel NoScript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=981817467469013&ev=PageView&noscript=1"
+          />
+        </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
